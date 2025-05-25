76f3492b4d68d32e975e928e43d596a79c0480d3
Bastykey <169182698+Bastykey@users.noreply.github.com>
2025-05-25 05:25:53 -0400

    Adding files
    Agregando microservicios restantes

---
+++ MicroservicioTiendas/src/test/java/cl/duoc/MicroservicioTiendas/MicroservicioTiendasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.MicroservicioTiendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTiendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
